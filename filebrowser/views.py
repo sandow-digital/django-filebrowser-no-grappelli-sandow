b1b2bfee4d780e269ea3274adc20127474b21be5
Chris Coleman <colemanc@colemanesolutions.com>
2011-11-10 00:27:07 -0700

    Fix GET request (hopefully)

--- filebrowser/views.py
+++ filebrowser/views.py
@@ -49,7 +49,7 @@ def browse(request):
 
     # Redirect for images to use business photos
     if request.GET.get('type') == 'image':
-        return HttpResponseRedirect("/admin/business/photo/", request.GET)
+        return HttpResponseRedirect("/admin/business/photo/", request.GET.copy())
  
     # QUERY / PATH CHECK
     query = request.GET.copy()
