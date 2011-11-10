95a82a4d1b77fa4815b04fa3e46578b18e602c85
Chris Coleman <colemanc@colemanesolutions.com>
2011-11-09 23:27:31 -0700

    Add photo selection support to filebrowser

--- filebrowser/media/filebrowser/js/FB_TinyMCE.js
+++ filebrowser/media/filebrowser/js/FB_TinyMCE.js
@@ -22,6 +22,26 @@ var FileBrowserDialogue = {
         // close popup window
         tinyMCEPopup.close();
     }
+    photoSubmit : function (PhotoURL, PhotoCaption, PhotoTitle) {
+        var URL = PhotoURL;
+        var win = tinyMCEPopup.getWindowArg("window");
+
+        // insert information now
+        win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = URL;
+        win.document.getElementById("alt") = PhotoCaption;
+        win.document.getElementById("title").value = PhotoTitle;
+
+        // change width/height & show preview
+        if (win.ImageDialog){
+            if (win.ImageDialog.getImageData)
+                win.ImageDialog.getImageData();
+            if (win.ImageDialog.showPreviewImage)
+                win.ImageDialog.showPreviewImage(URL);
+        }
+
+        // close popup window
+        tinyMCEPopup.close();
+    }
 }
 
 tinyMCEPopup.onInit.add(FileBrowserDialogue.init, FileBrowserDialogue);
